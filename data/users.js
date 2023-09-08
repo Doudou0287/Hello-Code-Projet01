dcca9b9428d6381b6d5e5e3fd28aa57738d85dd0
Sundus <116579651+Doudou0287@users.noreply.github.com>
2023-09-08 14:51:31 +0200

    add photos to articles

--- data/users.js
+++ data/users.js
@@ -7,3 +7,16 @@ export default [
   ];
   
   
+const  usersTable = [
+  {
+    name: "An",
+    email: "an@gmail.com",
+    password: "$2b$10$WiIzzTnkX0vOv3q7Yp.ISeo7NpMRHUV0PKr21zvNlBqD6slN10Tg.", // correspond à "123456"
+  },
+  {
+    name: "Admin",
+    email: "aadmin@gmail.com",
+    password: "$2b$10$c6WOzie10E02Wi4WJuIJcOYt4B5yh7M02dKyYQYiLxRUzMxTAixKW", // correspond à "sss"
+  }
+];
+db.users.insertMany(usersTable)
